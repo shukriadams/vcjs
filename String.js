3e2eb3c32865ad4fd976c144f9705f19c0abebcf
Shukri Adams <shukri.adams@gmail.com>
2013-10-05 15:00:24 +0200

    Added String extensions.

---
+++ String.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    "use strict";
+
+    // ======================================================
+    // adds string.format support
+    // ------------------------------------------------------
+    // from http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+    String.prototype.format = function () {
+        var formatted = this;
+        for (var i = 0; i < arguments.length; i++) {
+            var regexp = new RegExp('\\{' + i + '\\}', 'gi');
+            formatted = formatted.replace(regexp, arguments[i]);
+        }
+        return formatted;
+    };
+
+});
