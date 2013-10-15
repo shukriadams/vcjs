b2251d146a9df8db9a23c87e8d6edf405b5b9e4e
Shukri Adams <shukri.adams@gmail.com>
2013-10-15 21:15:17 +0200

    Moved modal and parser class in from hivefinder.

---
+++ vc.modal.js
@@ -0,0 +1,63 @@
+﻿$(function () {
+
+    "use strict";
+
+    window.vc = window.vc || {};
+    var vc = window.vc;
+
+
+    // ===============================================================================
+    // Constructor
+    // -------------------------------------------------------------------------------
+    vc.modal = function (options) {
+        var defaults = {
+            url: null,      // url to get modal content from
+            content: null,  // static modal content
+            title: "",
+            onLoaded : null // callback
+        };
+
+        options = $.extend(defaults, options);
+        var host = $('#modal-host');
+        this.host = host;
+        var self = this;
+
+        if (options.url) {
+            $.ajax({
+                url: options.url,
+                success: function (html) {
+                    self.body = host.find('[class="modal-body"]');
+                    self.body.html(html);
+                    $(host).modal('show');
+
+                    if (options.onLoaded)
+                        options.onLoaded({ modal: self });
+                }
+            });
+        } else if (options.content) {
+            host.find('[class="modal-body"]').html(options.content);
+            $(host).modal('show');
+        }
+
+        $(host).find('#btnCloseLoggerMain').click(function () {
+            $(host).modal('hide');
+        });
+
+        $(host).find('[class="modal-title"]').html(options.title);
+    };
+
+
+    // ===============================================================================
+    // 
+    // -------------------------------------------------------------------------------
+    vc.modal.prototype = function () { this.apply(this, arguments); };
+
+
+    // ===============================================================================
+    // 
+    // -------------------------------------------------------------------------------
+    vc.modal.prototype.close = function () {
+        this.host.modal('hide');
+    };
+
+});
---
+++ vc.parser.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+
+    "use strict";
+
+    window.peep = window.peep || {};
+    var vc = window.vc;
+    vc.parser = vc.parser || {};
+
+    vc.parser.returnAfterLast = function (main, sub) {
+        // if substring doesn't exist in main string, returns zero length string
+        if (main.indexOf(sub) == -1)
+            return "";
+
+        // if no text after substring, returns zero length string
+        if (main.length - 1 == main.lastIndexOf(sub))
+            return "";
+
+        return main.substring(main.lastIndexOf(sub) + sub.length);
+    };
+
+    vc.parser.returnUptoLast = function (main, sub) {
+        // if substring doesn't exist in main string, returns zero length string
+        if (main.indexOf(sub) == -1)
+            return "";
+
+        // if no text before substring, returns the main string
+        if (main.length == sub.length)
+            return "";
+
+        // if reaches here, proceed to find desired substring
+        return main.substring(0, main.lastIndexOf(sub));
+    };
+
+});
