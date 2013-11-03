5be0819638fe9b1e70b822d50ea77d99cf04b36b
Shukri Adams <shukri.adams@gmail.com>
2013-11-03 15:40:31 +0100

    Added hextorgb method.

--- vc.js
+++ vc.js
@@ -45,6 +45,23 @@
         return formatted;
     };
 
+    // ======================================================
+    // 
+    // ------------------------------------------------------
+    vc.hexFromRGB = function (r, g, b) {
+        var hex = [
+            r.toString(16),
+            g.toString(16),
+            b.toString(16)
+        ];
+        $.each(hex, function (nr, val) {
+            if (val.length === 1) {
+                hex[nr] = "0" + val;
+            }
+        });
+        return hex.join("").toUpperCase();
+    };
+
 
     // ======================================================
     // Bind "enter" key on a field to click a button
@@ -66,6 +83,9 @@
         });
     };
 
+    // ======================================================
+
+    // ======================================================
     vc.secondsToMinutes = function (seconds) {
         var minutes = Math.floor(seconds / 60);
 
