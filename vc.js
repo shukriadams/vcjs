20ed2636cb919fa80c89a3ff4b66ea89e6f202db
Shukri Adams <shukri.adams@gmail.com>
2014-10-15 18:12:23 +0200

    added guid generation method.

--- vc.js
+++ vc.js
@@ -78,6 +78,21 @@ define('vc', ['jquery'], function ($) {
 
 
     // ======================================================
+    // Genereates a GUID
+    // from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+    // ------------------------------------------------------
+    vc.generateUUID = function(){
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+        });
+        return uuid;
+    };
+
+
+    // ======================================================
     // checks if value is in inteCreate format
     // ------------------------------------------------------
     vc.isInt = function (value) {
